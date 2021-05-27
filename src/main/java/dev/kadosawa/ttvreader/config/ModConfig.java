2cd93dd0ac4328607398c80e638c63ab66394a14
howaitoreivun <72914371+howaitoreivun@users.noreply.github.com>
2021-05-28 00:28:00 +0400

    Implemented a little bit more settings

--- src/main/java/dev/kadosawa/ttvreader/config/ModConfig.java
+++ src/main/java/dev/kadosawa/ttvreader/config/ModConfig.java
@@ -2,10 +2,29 @@ package dev.kadosawa.ttvreader.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @SuppressWarnings("unused")
 @Config(name = "ttvreader")
 public class ModConfig implements ConfigData {
+    public boolean HideTwitchChat = false;
+
     public String username = "";
     public String OAuthToken = "";
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public Badges badges = new Badges();
+
+    public static class Badges {
+        public String moderators = "\uD83D\uDDE1";
+        public String subscribers = "\u2764";
+    }
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public Colors colors = new Colors();
+
+    public static class Colors {
+        public String moderators = "#00AD03";
+        public String subscribers = "red";
+    }
 }
