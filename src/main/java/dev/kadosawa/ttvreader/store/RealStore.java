30fba9a6bba51e5faa7b83fa3167f7442d9fdc50
httport <96192210+httport@users.noreply.github.com>
2021-05-29 00:57:18 +0400

    Re-created store using Java instead of Kotlin

---
+++ src/main/java/dev/kadosawa/ttvreader/store/RealStore.java
@@ -0,0 +1,59 @@
+package dev.kadosawa.ttvreader.store;
+
+import com.gikk.twirk.Twirk;
+import com.gikk.twirk.TwirkBuilder;
+import dev.kadosawa.ttvreader.config.ModConfig;
+import dev.kadosawa.ttvreader.listeners.ChatListener;
+import me.shedaniel.autoconfig.AutoConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+@SuppressWarnings("unused")
+public class RealStore {
+    @Nullable
+    private static String channelName = null;
+
+    @Nullable
+    private static Twirk twirk = null;
+
+    public static @Nullable String getChannelName() {
+        return channelName;
+    }
+
+    public static void setChannelName(@NotNull String value) {
+        channelName = value;
+    }
+
+    public static @Nullable Twirk getTwirk() {
+        return twirk;
+    }
+
+    public static void initialState() {
+        channelName = null;
+
+        if (twirk != null)
+            twirk.close();
+
+        twirk = null;
+    }
+
+    public static CompletableFuture<Boolean> createTwirkConnection() {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("Running in the 90's");
+
+            ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
+            twirk = new TwirkBuilder(channelName, config.username, config.OAuthToken).build();
+            twirk.addIrcListener(new ChatListener());
+
+            try {
+                return twirk.connect();
+            } catch (IOException | InterruptedException e) {
+                return false;
+            }
+        });
+    }
+}
